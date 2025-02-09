4a19b9014d4d6a89d9d133e97e11c9ae8d031612
Oscarraizer <boltenkovs36@gmail.com>
2025-02-09 22:10:40 +0700

    feat: adds vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@ import TheDescription from '@/components/TheDescription.vue'
 import SkillsSection from '@/components/SkillsSection.vue'
 import ProjectsSection from '@/components/ProjectsSection.vue'
 import ContactsSection from '@/components/ContactsSection.vue'
+import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <template>
+  <Analytics />
   <div
     class="bg-[#d6d5dd] md:h-screen p-3 font-demibold leading-none text-[#060218] md:overflow-hidden"
   >
