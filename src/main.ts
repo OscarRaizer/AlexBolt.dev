eeb00eedbc44538736539042ffa20e5b073c85d9
Oscarraizer <boltenkovs36@gmail.com>
2025-02-10 02:48:47 +0700

    feat: change title, adds google analytics

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { createGtm } from '@gtm-support/vue-gtm'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +10,13 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+  createGtm({
+    id: 'G-0V4KY4EBL8',
+    vueRouter: router,
+    enabled: true,
+    debug: false,
+  }),
+)
 
 app.mount('#app')
