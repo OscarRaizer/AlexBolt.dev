ab44c0e7606df79710a302e86c61af05a5441e1d
Oscarraizer <boltenkovs36@gmail.com>
2025-02-09 23:22:18 +0700

    feat: adds decription

--- src/components/TheDescription.vue
+++ src/components/TheDescription.vue
@@ -5,10 +5,29 @@
       <h2 class="tracking-tighter text-wrap text-title-title">Frontend</h2>
       <h2 class="tracking-tighter text-wrap text-title-title">Engineer</h2>
     </div>
-    <p class="font-medium text-2xl leading-none text-decription">
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-      been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-      galley of type and scrambled it to make a type specimen book.
-    </p>
+    <div class="font-medium leading-7 text-decription">
+      <p>Building web applications from sunny Bangkok.</p>
+      <p>
+        My journey started in IT support, evolving into creating fast, engaging, and thoughtfully
+        designed applications since 2022.
+      </p>
+      <p>
+        When I'm not coding, I’m cruising the city on a motorbike,
+        <a
+          class="underline hover:bg-[#060218] hover:text-[#d6d5dd]"
+          href="https://www.instagram.com/boltenkov.pic/"
+          target="_blank"
+          >capturing moments</a
+        >, or lost in
+        <a
+          class="underline hover:bg-[#060218] hover:text-[#d6d5dd]"
+          href="https://www.youtube.com/@oscarraizer4869/videos"
+          target="_blank"
+        >
+          music.
+        </a>
+      </p>
+      <p>It's all about finding beauty in both pixels and real life.</p>
+    </div>
   </article>
 </template>
